6e8d2b9faeed6be361b7f5acdc68692461b6db81
eademidova <eademid@gmail.com>
2026-03-15 15:47:32 +0300

    add presentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# [](gitverse.ru/eademidova/study_2025-2026_mathmod/compare/v1.1.0...v) (2026-03-15)
+
+
+
 #  (2026-02-11)
 
 
@@ -6,4 +10,5 @@
 * **main:** make course structure ([5c5192f](https://github.com/eademidova/study_2024-2025_mathmod/commit/5c5192f6385cf0f92e4fc60f507a7c115f0921d2))
 
 
+### [1.0.1](https://gitverse.ru/eademidova/study_2025-2026_mathmod/compare/v1.0.0...v1.0.1) (2026-02-18)
 
---
+++ labs/lab02/project/scripts/convergence.jl
@@ -0,0 +1,42 @@
+using DrWatson
+@quickactivate "project"
+using Distributions, Plots, Statistics, Random, JLD2
+# Параметры модели
+λ = 5.0
+theor_prob = 1 - cdf(Poisson(λ), 10)
+# Размеры выборки для исследования (логарифмическая шкала)
+sample_sizes = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]
+# Для воспроизводимости
+Random.seed!(123)
+# Вычисляем эмпирические оценки для каждого объёма выборки
+estimates = Float64[]
+println("Вычисление оценок вероятности...")
+for n in sample_sizes
+hourly_sample = rand(Poisson(λ), n)
+emp_prob = count(hourly_sample .> 10) / n
+push!(estimates, emp_prob)
+println("n = $n: оценка = $emp_prob")
+end
+# Построение графика сходимости
+p = plot(sample_sizes, estimates,
+xscale = :log10,
+marker = :circle,
+label = "Эмпирическая оценка",
+xlabel = "Объём выборки (часы)",
+ylabel = "Оценка вероятности P(>10)",
+legend = :bottomright)
+hline!(p, [theor_prob],
+label = "Теоретическое значение",
+ls = :dash,
+lw = 2,
+color = :red)
+title!(p, "Сходимость оценки вероятности P(>10) при λ=$λ")
+# Сохраняем график в папку plots
+plot_path = plotsdir("convergence.png")
+savefig(p, plot_path)
+println("График сохранён в $plot_path")
+# Сохраняем данные сходимости в JLD2 (опционально)
+data_path = datadir("convergence", "convergence_data_λ=$(λ).jld2")
+mkpath(datadir("convergence"))
+@save data_path sample_sizes estimates λ theor_prob
+println("Данные сходимости сохранены в $data_path")
